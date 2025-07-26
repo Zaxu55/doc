1cc83a7722e49303de78332d83daf4983d2e2a13
Zaxu55 <zaxmer5@gmail.com>
2025-07-26 13:01:36 +0530

    doc.php

--- doc.php
+++ doc.php
@@ -10,7 +10,7 @@ if (!$playerId) {
     exit;
 }
 
-$nonce = 'f0865f39b8';  // 🔁 Daily update karna
+$nonce = 'bb48ebcdde';  // 🔁 Daily update karna
 $url = "https://smartucshop.com/wp-admin/admin-ajax.php?action=bgmi_api_check&player_id=" . urlencode($playerId) . "&nonce=" . $nonce;
 
 $response = @file_get_contents($url);
