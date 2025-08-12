9d8003cc9793964dc97dc00964032a9923bec2cc
Zaxu55 <zaxmer5@gmail.com>
2025-08-12 06:14:44 +0530

    doc.php

--- doc.php
+++ doc.php
@@ -10,7 +10,7 @@ if (!$playerId) {
     exit;
 }
 
-$nonce = 'bb48ebcdde';  // 🔁 Daily update karna
+$nonce = '4c02f3647c';  // 🔁 Daily update karna
 $url = "https://smartucshop.com/wp-admin/admin-ajax.php?action=bgmi_api_check&player_id=" . urlencode($playerId) . "&nonce=" . $nonce;
 
 $response = @file_get_contents($url);
