80d945f792772e5c449ff34ff72247d1f8c86169
Zaxu55 <zaxmer5@gmail.com>
2025-08-15 00:00:30 +0530

    doc.php

--- doc.php
+++ doc.php
@@ -10,7 +10,7 @@ if (!$playerId) {
     exit;
 }
 
-$nonce = '1962e5fb1c';  // 🔁 Daily update karna
+$nonce = 'b8059d87ac';  // 🔁 Daily update karna
 $url = "https://smartucshop.com/wp-admin/admin-ajax.php?action=bgmi_api_check&player_id=" . urlencode($playerId) . "&nonce=" . $nonce;
 
 $response = @file_get_contents($url);
